134c41e58f311d7a0637527db5f1f93e2363b945
darmiel <71837281+darmiel@users.noreply.github.com>
2023-11-01 00:33:35 +0100

    fix: auto convert to RGB

--- api.py
+++ api.py
@@ -17,11 +17,11 @@ def index():
 
 @app.route("/upload", methods=["POST"])
 def upload():
-    # read parameters
     try:
         quality = int(request.form.get('quality', '10'))
+        watermark_size = int(request.form.get('watermark_size', '18'))
     except:
-        return abort(400, "Cannot parse quality")
+        return abort(400, "Cannot parse parameter/s")
     randomize_position = request.form.get('randomize', 'off') == "on"
     watermark = request.form.get('watermark', 'off') == "on"
 
@@ -32,32 +32,43 @@ def upload():
     try:
         img = Image.open(body_image.stream)
     except:
-        return abort(400, "the file you uploaded does not appear to be a valid image.")
+        return abort(400, "The file you uploaded does not appear to be a valid image.")
+
+    print("[Info]", request.remote_addr, "requested to enhance '", body_image.name, "' to quality =", quality, "with watermark/randomize =", watermark, randomize_position)
+
+    # discard alpha channel (since JPEG can't handle alpha)
+    if img.mode != "RGB":
+        print("  [Debug] The uploaded image is not RGB. trying to convert.")
+        try:
+            img = img.convert("RGB")
+        except:
+            return abort(400, "cannot convert image to RGB")
 
     # add watermark
     if watermark:
         draw = ImageDraw.Draw(img)
         text = "jpeg.qwer.tz"
-        font = ImageFont.truetype("arial.ttf", 18)
+        font = ImageFont.truetype("arial.ttf", watermark_size)
         
         text_width, text_height = draw.textsize(text, font)
 
-        print(randomize_position)
         if randomize_position:
             text_position = (randint(20, img.width - text_width - 20), randint(10, img.height - text_height - 10))
         else:
             text_position = ((img.width - text_width) - 20, (img.height - text_height) - 10)
+        print("  [Debug] Watermark position:", text_position)
             
         background_position = (text_position[0] - 10, text_position[1] - 5)
         background_dimensions = (text_width + 20, text_height + 10)
 
         draw.rectangle([background_position, (background_position[0] + background_dimensions[0], background_position[1] + background_dimensions[1])], fill="red")
-        draw.text(text_position, text, font=font, fill="yellow")  # White text
+        draw.text(text_position, text, font=font, fill="yellow")
 
     # "enhance" quality and return
     out = BytesIO()
     img.save(out, 'JPEG', quality=quality)
     out.seek(0)
+    print("  [Debug] Image enhanced to", out.getbuffer().nbytes, "bytes")
 
     return send_file(out, mimetype="image/jpeg")
 
