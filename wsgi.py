00f6f80da959e058b463fd11e54b5d5deeb67580
darmiel <71837281+darmiel@users.noreply.github.com>
2023-11-01 00:38:10 +0100

    fix: proxy ip

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,7 @@
 from api import app
 
+from werkzeug.middleware.proxy_fix import ProxyFix
+
 if __name__ == "__main__":
+    app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
     app.run()
