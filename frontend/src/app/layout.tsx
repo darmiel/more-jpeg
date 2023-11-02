7be1497365db4e1bf3b7b41b3a35bcea5e2ccfa3
darmiel <71837281+darmiel@users.noreply.github.com>
2023-11-02 11:27:43 +0100

    frontend: initial commit

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,25 @@
+import { Providers } from "@/app/providers";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "more-jpeg",
+  description: "Instantly make your images a lot worse!",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className="dark">
+      <body className={inter.className}>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  );
+}
