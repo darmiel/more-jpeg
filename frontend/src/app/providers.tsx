31cce72db9afdfa5a7f00967b53c477921fea1a9
darmiel <71837281+darmiel@users.noreply.github.com>
2023-11-04 15:08:48 +0100

    refactor: updated nextui

--- frontend/src/app/providers.tsx
+++ frontend/src/app/providers.tsx
@@ -2,10 +2,13 @@
 
 import { SearchProvider } from "@/context/SearchContext"
 import { NextUIProvider } from "@nextui-org/react"
+import { useRouter } from "next/navigation"
 
 export function Providers({ children }: { children: React.ReactNode }) {
+  const router = useRouter()
+
   return (
-    <NextUIProvider>
+    <NextUIProvider navigate={router.push}>
       <SearchProvider>{children}</SearchProvider>
     </NextUIProvider>
   )
